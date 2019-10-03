18ff75cfacbba890e2666f028a26b9f01a3f4b9a
Gyurcsik Dóra <gyurcsik.dora@gmail.com>
2019-10-03 15:42:16 +0200

    get message header

--- dicewarsSpring/src/main/java/dicewarsSpring/hello/WebSocketConfiguration.java
+++ dicewarsSpring/src/main/java/dicewarsSpring/hello/WebSocketConfiguration.java
@@ -22,4 +22,5 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
                 .enableSimpleBroker("/chat");
     }
 
+
 }
--- dicewarsSpring/src/main/java/dicewarsSpring/hello/WebSocketController.java
+++ dicewarsSpring/src/main/java/dicewarsSpring/hello/WebSocketController.java
@@ -6,9 +6,13 @@ import dicewarsSpring.Model.Field;
 import dicewarsSpring.Service.BoardService;
 import dicewarsSpring.Service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.server.ResponseStatusException;
 
 @Controller
@@ -24,10 +28,45 @@ public class WebSocketController {
 
     @SendTo("/chat")
     @MessageMapping("/send/message")
-    public String onReceivedMessage(String message) {
+    public String onReceivedMessage(Message message, String messageS) {
+        MessageHeaders headers = message.getHeaders();
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+        MultiValueMap<String, String> multiValueMap = headers.get(StompHeaderAccessor.NATIVE_HEADERS, MultiValueMap.class);
+        String header = multiValueMap.get("header").get(0);
+        if (header.equals("war")) {
+            Gson gson = new Gson();
+            Field[] war = gson.fromJson(messageS, Field[].class);
+            try {
+                int[] points = gameService.attack(board.getBoard()[war[0].getRow()][war[0].getCol()], board.getBoard()[war[1].getRow()][war[1].getCol()]);
+                String json = new Gson().toJson(points);
+                return json;
+            } catch (ResponseStatusException e) {
+                return new Gson().toJson(e);
+            }
+        }
+        if (header.equals("getBoard")) {
+            Field[] board = boardService.getBoard();
+            String json = new Gson().toJson(board);
+            return json;
+        }
+        if (header.equals("initBoard")) {
+            Field[] board = boardService.initializeBoard();
+            String json = new Gson().toJson(board);
+            return json;
+        }
+        if (header.equals("whosTurn")) {
+            if (messageS.equals("true")) {
+                gameService.endOfTurn(true);
+                return "true";
+            } else {
+                gameService.endOfTurn(false);
+                return "false";
+            }
+        }
+
         //this.template.convertAndSend("/chat", new SimpleDateFormat("HH:mm:ss").format(new Date()) + "- " + message);
         //MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
-        if (message.startsWith("[")) {
+        /*if (message.startsWith("[")) {
             Gson gson = new Gson();
             Field[] war = gson.fromJson(message, Field[].class);
             try {
@@ -56,6 +95,8 @@ public class WebSocketController {
             gameService.endOfTurn(false);
             return "false";
         }
+        return null;*/
+
         return null;
     }
 
